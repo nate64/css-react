c5de1c7fc3d52a3080b43858158278cf7558d9af
Nate <nathanestes@gmail.com>
2018-11-27 11:08:48 -0700

    nate was here

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <p>Nate Was Here.
+            
+          </p>
           <a
             className="App-link"
             href="https://reactjs.org"
